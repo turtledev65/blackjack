b5d51c5c667634e5709cef6791661d03ab412f45
turtledev <turtledev65@gmail.com>
2024-01-17 17:24:20 +0200

    [frontend] feat: create component for rendering cards

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,16 @@
+import Card from "./components/card";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-400 underline">Hello world!</h1>
+    <>
+      <div className="flex min-h-screen items-center justify-center gap-2">
+        <Card value={5} type="clubs" />
+        <Card value={11} type="spades" />
+        <Card value={12} type="clubs" />
+        <Card value={13} type="diamonds" />
+        <Card value={14} type="hearts" />
+      </div>
+    </>
   );
 }
 
---
+++ app/src/components/card.tsx
@@ -0,0 +1,59 @@
+type CardType = "clubs" | "diamonds" | "hearts" | "spades";
+
+type CardProps = {
+  value: number;
+  type: CardType;
+};
+
+import {
+  ImClubs as Clubs,
+  ImHeart as Hearts,
+  ImDiamonds as Diamonds,
+  ImSpades as Spades
+} from "react-icons/im";
+
+const Card = ({ value, type }: CardProps) => {
+  const Icon = (() => {
+    console.log("Icon called with type", type);
+    if (type === "clubs") return Clubs;
+    else if (type === "diamonds") return Diamonds;
+    else if (type === "spades") return Spades;
+    else return Hearts;
+  })();
+  const label = (() => {
+    if (value <= 10) return value;
+    if (value === 11) return "J";
+    else if (value === 12) return "Q";
+    else if (value === 13) return "K";
+    else return "A";
+  })();
+
+  return (
+    <div className="aspect-2/3 w-44 rounded bg-white p-2 text-xl">
+      <div className="relative h-full w-full">
+        <div
+          className={`absolute left-0 top-0 flex flex-col items-center justify-center font-bold ${
+            type === "hearts" || type === "diamonds"
+              ? "text-red-500"
+              : "text-black"
+          }`}
+        >
+          {label}
+          <Icon />
+        </div>
+        <div
+          className={`absolute bottom-0 right-0 flex rotate-180 flex-col items-center justify-center font-bold ${
+            type === "hearts" || type === "diamonds"
+              ? "text-red-500"
+              : "text-black"
+          }`}
+        >
+          {label}
+          <Icon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
