6cd841c7f761ee6b5ba7c4f3cf89d0513012699c
turtledev <turtledev65@gmail.com>
2024-01-17 17:23:53 +0200

    [frontend] style: add custom aspect ratio for tailwind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      aspectRatio: {
+        "2/3": "2 / 3"
+      }
+    }
   },
   plugins: []
 };
