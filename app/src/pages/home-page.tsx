969ddf2cf60af58921da43abfc2c065b6cafd422
turtledev <turtledev65@gmail.com>
2024-01-21 22:06:19 +0200

    [frontend] fix: make the buttons in the homepage links so they redirect to the correct page

--- app/src/pages/home-page.tsx
+++ app/src/pages/home-page.tsx
@@ -1,17 +1,28 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-20 px-5 font-bold text-white">
-      <h1 className="text-center text-6xl">Blackjack</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-20 px-5 text-center font-bold text-white">
+      <h1 className="text-6xl">Blackjack</h1>
       <div className="flex w-full max-w-xl flex-col justify-center gap-6">
-        <button className="rounded bg-blue-500 py-2 text-xl hover:bg-blue-600">
+        <Link
+          to="create-room"
+          className="rounded bg-blue-500 py-2 text-xl hover:bg-blue-600"
+        >
           Create Room
-        </button>
-        <button className="rounded bg-green-500 py-2 text-xl hover:bg-green-600">
+        </Link>
+        <Link
+          to="join-room"
+          className="rounded bg-green-500 py-2 text-xl hover:bg-green-600"
+        >
           Join Room
-        </button>
-        <button className="rounded bg-gray-400 py-2 text-xl hover:bg-gray-500">
+        </Link>
+        <Link
+          to="/"
+          className="rounded bg-gray-400 py-2 text-xl hover:bg-gray-500"
+        >
           About
-        </button>
+        </Link>
       </div>
     </div>
   );
