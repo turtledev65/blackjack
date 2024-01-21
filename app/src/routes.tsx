2a3783afcf5c63dae3f98457d851522aa2b459d4
turtledev <turtledev65@gmail.com>
2024-01-21 21:48:54 +0200

    [frontent] chore: create the create-room page

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout";
 import TestPage from "./pages/test-page";
 import HomePage from "./pages/home-page";
+import CreateRoomPage from "./pages/create-room-page";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: "create-room", element: <CreateRoomPage /> },
       { path: "test", element: <TestPage /> }
     ]
   }
