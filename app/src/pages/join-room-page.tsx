e62ce12d0576aaad81e900ad357a61bfd72a2729
turtledev <turtledev65@gmail.com>
2024-01-21 21:57:05 +0200

    [frontend] feat: add join room page

---
+++ app/src/pages/join-room-page.tsx
@@ -0,0 +1,24 @@
+const JoinRoomPage = () => {
+  return (
+    <form className="flex min-h-screen flex-col items-center justify-center gap-6 text-xl text-white">
+      <div className="space-x-2">
+        <label className="text-2xl" htmlFor="name">
+          Name:
+        </label>
+        <input
+          className="border-b-2 bg-transparent outline-none"
+          id="name"
+          type="text"
+        />
+      </div>
+      <button
+        type="submit"
+        className="rounded bg-blue-500 px-2 py-3 font-bold hover:bg-blue-600"
+      >
+        Join Room
+      </button>
+    </form>
+  );
+};
+
+export default JoinRoomPage;
--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layout";
 import TestPage from "./pages/test-page";
 import HomePage from "./pages/home-page";
 import CreateRoomPage from "./pages/create-room-page";
+import JoinRoomPage from "./pages/join-room-page";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: "create-room", element: <CreateRoomPage /> },
+      { path: "join-room", element: <JoinRoomPage /> },
       { path: "test", element: <TestPage /> }
     ]
   }
