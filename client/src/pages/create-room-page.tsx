7b5a025bc4370301ef4efbfb83d0dd133a4754e5
turtledev <turtledev65@gmail.com>
2024-01-22 23:30:11 +0200

    [frontend] feat: send the websocket event to create a room when submitting the form to create one

--- client/src/pages/create-room-page.tsx
+++ client/src/pages/create-room-page.tsx
@@ -1,6 +1,17 @@
+import React, { useEffect, useRef } from "react";
+import socket from "../utils/socket";
 const CreateRoomPage = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const handleCreateRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const gameName = nameRef.current?.value?.trim();
+    if (gameName) socket.emit("create-room", gameName);
+  };
   return (
-    <form className="flex min-h-screen flex-col items-center justify-center gap-6 text-xl text-white">
+    <form
+      onSubmit={handleCreateRoom}
+      className="flex min-h-screen flex-col items-center justify-center gap-6 text-xl text-white"
+    >
       <div className="space-x-2">
         <label className="text-2xl" htmlFor="name">
           Name:
@@ -9,6 +20,7 @@ const CreateRoomPage = () => {
           className="border-b-2 bg-transparent outline-none"
           id="name"
           type="text"
+          ref={nameRef}
         />
       </div>
       <button
