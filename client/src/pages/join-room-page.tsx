a38ecddf4e6f2d9e25834b336f89460e1003d21c
turtledev <turtledev65@gmail.com>
2024-01-23 23:54:29 +0200

    [frontend] feat: make it possible to joina room in the room/join route

--- client/src/pages/join-room-page.tsx
+++ client/src/pages/join-room-page.tsx
@@ -1,6 +1,33 @@
+import { useEffect, useRef } from "react";
+import socket from "../utils/socket";
+import { useNavigate } from "react-router-dom";
+
 const JoinRoomPage = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    socket.on("room-joined", name => {
+      navigate(`/room/${name}`);
+    });
+
+    return () => {
+      socket.off("room-joined");
+    };
+  }, []);
+
+  const handleJoinRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const gameName = nameRef.current?.value?.trim();
+    console.log(gameName);
+    if (gameName) socket.emit("join-room", gameName);
+  };
+
   return (
-    <form className="flex min-h-screen flex-col items-center justify-center gap-6 text-xl text-white">
+    <form
+      onSubmit={handleJoinRoom}
+      className="flex min-h-screen flex-col items-center justify-center gap-6 text-xl text-white"
+    >
       <div className="space-x-2">
         <label className="text-2xl" htmlFor="name">
           Name:
@@ -9,6 +36,7 @@ const JoinRoomPage = () => {
           className="border-b-2 bg-transparent outline-none"
           id="name"
           type="text"
+          ref={nameRef}
         />
       </div>
       <button
