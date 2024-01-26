e1c8ec03cea05033a53abf7173fbf067326138d4
turtledev <turtledev65@gmail.com>
2024-01-26 21:28:28 +0200

    [frontend] chore: remove unused variable

--- client/src/pages/game-page.tsx
+++ client/src/pages/game-page.tsx
@@ -1,9 +1,7 @@
-import { useParams } from "react-router-dom";
 import { useState } from "react";
 import socket from "../utils/socket";
 
 const GamePage = () => {
-  const { name } = useParams();
 
   return <div>Game: {name}</div>;
 type BetFormProps = {
