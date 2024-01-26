16b3fc198a937aa4aaa69e3956d6a9a77b04d289
turtledev <turtledev65@gmail.com>
2024-01-26 22:16:35 +0200

    [frontned] style: change how the toast error notification look

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -6,7 +6,14 @@ import socket from "./utils/socket";
 const Layout = () => {
   useEffect(() => {
     socket.on("error", msg => {
-      toast.error(msg, { duration: 3000 });
+      toast.error(msg, {
+        duration: 3000,
+        className: "bg-red-500 text-white text-xl font-bold",
+        iconTheme: {
+          primary: "white",
+          secondary: "rgb(239 68 68)"
+        }
+      });
     });
 
     return () => {
