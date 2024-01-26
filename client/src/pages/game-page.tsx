dba726550d1af4174647f27fd33cfe70c2455ac9
turtledev <turtledev65@gmail.com>
2024-01-26 20:37:23 +0200

    [fronent] feat: create form for submitting a bet

--- client/src/pages/game-page.tsx
+++ client/src/pages/game-page.tsx
@@ -1,9 +1,46 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const GamePage = () => {
   const { name } = useParams();
 
   return <div>Game: {name}</div>;
+type BetFormProps = {
+  minAmmount: number;
+  maxAmmount: number;
+};
+
+const BetForm = ({ minAmmount, maxAmmount }: BetFormProps) => {
+  const [ammount, setAmmount] = useState(minAmmount);
+
+  return (
+    <form>
+      <div className="inline-flex select-none flex-row items-center bg-white text-center  font-bold">
+        <div className="px-2 text-center text-3xl tabular-nums">${ammount}</div>
+        <div className="flex flex-col bg-green-600 px-2  text-white">
+          <button
+            type="button"
+            onClick={() =>
+              setAmmount(prevAmmount => Math.min(prevAmmount + 5, maxAmmount))
+            }
+            className="text-2xl"
+          >
+            +
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              setAmmount(prevAmount => Math.max(prevAmount - 5, minAmmount))
+            }
+            className="text-4xl"
+          >
+            -
+          </button>
+        </div>
+      </div>
+      <button type="submit">OK</button>
+    </form>
+  );
 };
 
 export default GamePage;
