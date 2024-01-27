b7050cbc0433b5ab4cd5b600ea7d45f90b204d2f
turtledev <turtledev65@gmail.com>
2024-01-27 14:31:06 +0200

    [backend] revert: remove useless functionality for creating a deck containing jockers

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 import redis from "./redis.js";
 
-function generateDeck(containsJockers = false) {
+function generateDeck() {
   const DECK_LENGTH = 52;
   const CARDS_PER_SUIT = 13;
   const types = ["clubs", "spades", "diamonds", "hearts"];
@@ -10,8 +10,6 @@ function generateDeck(containsJockers = false) {
     value: (index % CARDS_PER_SUIT) + 1, // make the value wrap between 1 and 13 based on the index
     type: types[index % types.length], // set the type based on the current index
   }));
-  if (containsJockers)
-    out.push({ value: 5, type: "jocker" }, { value: 10, type: "jocker" });
 
   // shuffle the deck
   out.sort((_a, _b) => Math.random() - 0.5);
