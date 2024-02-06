6eb9373a5e49a29293100c543fc968de631c7d9f
turtledev <turtledev65@gmail.com>
2024-02-06 16:47:29 +0200

    [frontend] revert: remove useless rotation prop from the card component

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -8,10 +8,9 @@ import { Card as CardType } from "../types";
 
 type CardProps = {
   card: CardType;
-  rotation?: number;
 };
 
-const Card = ({ card, rotation }: CardProps) => {
+const Card = ({ card }: CardProps) => {
   const Icon = (() => {
     if (card.type === "clubs") return Clubs;
     else if (card.type === "diamonds") return Diamonds;
@@ -27,10 +26,7 @@ const Card = ({ card, rotation }: CardProps) => {
   })();
 
   return (
-    <div
-      className="aspect-2/3 w-44 rounded bg-white p-2 text-xl shadow-2xl"
-      style={{ transform: `rotate(${rotation}deg)` }}
-    >
+    <div className="aspect-2/3 w-44 rounded bg-white p-2 text-xl shadow-2xl">
       <div className="relative flex h-full w-full items-center justify-center">
         <div
           className={`absolute left-0 top-0 flex flex-col items-center justify-center font-bold ${
