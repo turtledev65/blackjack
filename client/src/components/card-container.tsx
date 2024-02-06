7d01f532b8c3dc7bf4e9cca17ef698cac71f81cf
turtledev <turtledev65@gmail.com>
2024-02-06 20:57:13 +0200

    [frontend] feat: make it possible to also have flipped cards in a card container

--- client/src/components/card-container.tsx
+++ client/src/components/card-container.tsx
@@ -4,30 +4,42 @@ import CardComponent from "./card";
 
 type CardContainerProps = {
   cards: Card[];
+  flippedCards?: number;
 };
 
-const CardContainer = ({ cards }: CardContainerProps) => {
+const CardContainer = ({ cards, flippedCards = 0 }: CardContainerProps) => {
   if (!cards.length) {
     return (
       <div className="aspect-2/3 w-44 rounded border-8 border-dashed"></div>
     );
   }
 
+  const allCards = new Array(flippedCards + cards.length)
+    .fill(null)
+    .map((_, index) => (
+      <div
+        className="absolute top-0"
+        style={{
+          left: `${index * 2.5}rem`,
+          rotate: `${randomRange(-3, 3)}deg`
+        }}
+        key={index}
+      >
+        {index < flippedCards ? (
+          <CardComponent flipped />
+        ) : (
+          <CardComponent
+            value={cards[index - flippedCards].value}
+            type={cards[index - flippedCards].type}
+          />
+        )}
+      </div>
+    ));
+
   return (
     <div className="relative inline-block">
       <div className="invisible aspect-2/3 w-44"></div>
-      {cards.map((card, index) => (
-        <div
-          className="absolute top-0"
-          style={{
-            left: `${index * 2.5}rem`,
-            rotate: `${randomRange(-3, 3)}deg`
-          }}
-          key={index}
-        >
-          <CardComponent value={card.value} type={card.type} />
-        </div>
-      ))}
+      {...allCards}
     </div>
   );
 };
