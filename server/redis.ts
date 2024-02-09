465fc15ebd1f24c3b1dee745661ed35b1162b17f
turtledev <turtledev65@gmail.com>
2024-02-09 20:23:09 +0200

    [backend] chore: console.log when the redis client connects successfully

--- server/redis.ts
+++ server/redis.ts
@@ -1,6 +1,7 @@
 import { createClient } from "redis";
 
 const client = await createClient()
+  .on("connect", () => console.log("Redis client started successfully"))
   .on("error", (err: any) =>
     console.error("Redis client error:", err, typeof err),
   )
