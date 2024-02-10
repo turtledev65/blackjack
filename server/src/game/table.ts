e4d4fa42631d605968342c423def85aea22d989c
turtledev <turtledev65@gmail.com>
2024-02-10 18:12:09 +0200

    [backend] chore: add a name property to the table class

--- server/src/game/table.ts
+++ server/src/game/table.ts
@@ -2,10 +2,13 @@ import Deck from "./deck.js";
 import Player from "./player.js";
 
 export default class Table {
+  name: string;
   deck: Deck;
+
   private players: Player[] = [];
 
-  constructor() {
+  constructor(name: string) {
+    this.name = name;
     this.deck = new Deck();
   }
 
