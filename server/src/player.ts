9b7d8546fc6fb5506b1b199dfa76de4578825d67
turtledev <turtledev65@gmail.com>
2024-02-10 17:12:49 +0200

    [backend] chore: create a base player class

---
+++ server/src/player.ts
@@ -0,0 +1,27 @@
+import { Card } from "./types.js";
+
+export default class Player {
+  cards: Card[];
+
+  private _ballance: number = 0;
+  private _bet: number = 0;
+
+  constructor(cards: Card[], wallet: number) {
+    this.cards = [...cards];
+    this._ballance = Math.abs(wallet);
+  }
+
+  get ballance() {
+    return this._ballance;
+  }
+  set ballance(value) {
+    this._ballance = Math.max(this._ballance - value, 0);
+  }
+
+  get bet() {
+    return this._bet;
+  }
+  set bet(value) {
+    this._bet = Math.min(this._bet, this._ballance);
+  }
+}
