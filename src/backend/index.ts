8256dfcf66b4db7e170df6a947ea0d16ef836332
turtledev <turtledev65@gmail.com>
2024-02-20 10:56:10 +0200

    [backend] refactor: handle errors in a easier way

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,5 @@
 import { Server } from "socket.io";
 import Room from "./game/room";
-import { ApiResponse } from "../types";
 
 const io = new Server(3000, {
   cors: {
@@ -8,32 +7,29 @@ const io = new Server(3000, {
   }
 });
 
-console.log("backend started");
-
-function callThrowableFunction<T>(
-  callback: () => T,
-  responseCallback: (res: ApiResponse<T | null>) => unknown
-) {
-  try {
-    const res = callback();
-    responseCallback({ value: res, err: null });
-  } catch (err) {
-    error(err, responseCallback);
-  }
-}
-
-function error(err: unknown, cb: (res: ApiResponse<null>) => unknown) {
-  if (err instanceof Error) cb({ value: null, err: err.message });
-  else if (typeof err === "string") cb({ value: null, err });
-}
-
 const rooms = new Map<string, Room>();
 io.on("connection", socket => {
-  let currentRoom: Room | null;
+  let currentRoom: Room | null = null;
+
+  function handleError(err: unknown) {
+    console.log("err");
+    if (err instanceof Error) {
+      console.info("err", err);
+      socket.emit("err", err.message);
+    } else {
+      console.info(err);
+      socket.emit("err", "An unexpected err occured");
+    }
+  }
+
+  socket.on("room-exists", (name: string, cb) => {
+    if (rooms.has(name)) cb(true);
+    else cb(false);
+  });
 
   socket.on("create-room", (name: string, cb) => {
     if (rooms.has(name)) {
-      error(`Room ${name} already exists`, cb);
+      handleError(`Room ${name} already exists`);
       return;
     }
 
@@ -41,108 +37,129 @@ io.on("connection", socket => {
     rooms.set(currentRoom.name, currentRoom);
 
     currentRoom.addPlayer(socket);
-    cb({ value: name });
+    socket.join(currentRoom.name);
+
+    cb(currentRoom.name);
   });
 
   socket.on("join-room", (name, cb) => {
     const room = rooms.get(name);
     if (!room) {
-      error(`Could not find room ${name}`, cb);
+      handleError(`Could not find room ${name}`);
       return;
     }
 
-    callThrowableFunction(() => {
+    try {
       room.addPlayer(socket);
       currentRoom = room;
-      return room.name;
-    }, cb);
+      cb(name);
+    } catch (err) {
+      handleError(err);
+    }
   });
 
-  socket.on("bet", (value, cb) => {
+  socket.on("bet", value => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
     value = Number(value);
     if (isNaN(value)) {
       socket.emit("err", `Please enter a valid value`);
-      error("Please enter a valid value", cb);
+      handleError("Please enter a valid value");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.placeBet(value, socket);
-    }, cb);
+    try {
+      currentRoom.placeBet(value, socket);
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("hit", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.hitPlayer(socket);
-    }, cb);
+    try {
+      currentRoom.hitPlayer(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("stand", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.standPlayer(socket);
-    }, cb);
+    try {
+      currentRoom.standPlayer(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("double-down", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.doubleDown(socket);
-    }, cb);
+    try {
+      currentRoom.doubleDown(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("insurance", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.placeInsurance(socket);
-    }, cb);
+    try {
+      currentRoom.placeInsurance(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("surrender", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
+      cb();
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.surrender(socket);
-    }, cb);
+    try {
+      currentRoom.surrender(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 
   socket.on("split-pairs", cb => {
     if (!currentRoom) {
-      error("You have not joined a room yet", cb);
+      handleError("You have not joined a room yet");
       return;
     }
 
-    callThrowableFunction(() => {
-      currentRoom!.splitPairs(socket);
-    }, cb);
-  });
-
-  socket.onAny(() => {
-    console.log("Current Room", currentRoom);
+    try {
+      currentRoom.splitPairs(socket);
+      cb("test");
+    } catch (err) {
+      handleError(err);
+    }
   });
 });
